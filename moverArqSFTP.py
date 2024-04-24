777c3e664f4cefb1e8672459c1ce05599ae69cb6
prpirolo <prpirolo@gmail.com>
2024-04-24 17:07:09 -0300

    incluido o servico de log para mostrar se o programa está sendo executado com sucesso.

--- moverArqSFTP.py
+++ moverArqSFTP.py
@@ -1,3 +1,7 @@
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 import paramiko
 import json
 import fDados
@@ -5,68 +9,74 @@ import datetime as dt
 import os
 
 
+# Função para registrar mensagens de erro em um arquivo de log
+def log_error(error_message):
+    arqLog = 'Log-' + dt.datetime.today().strftime('%y%m%d') + '.txt'
+    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
+        arqlog.write(str(dt.datetime.now().strftime('%d/%m/%y %H:%M')) + ' - ERRO: ' + error_message + '\n')
+
+
+# Função para registrar mensagens de log
+def log_message(message):
+    arqLog = 'Log-' + dt.datetime.today().strftime('%y%m%d') + '.txt'
+    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
+        arqlog.write(str(dt.datetime.now().strftime('%d/%m/%y %H:%M')) + ' - ' + message + '\n')
+
+
 def test_ssh_connection(hostname, port, username, private_key_path, passphrase, password):
     try:
-        # Carregar a chave privada
         private_key = paramiko.RSAKey.from_private_key_file(private_key_path, password=passphrase)
-
-        # Criar cliente SSH
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        # Conectar ao servidor SSH
         client.connect(hostname=hostname, port=port, username=username, pkey=private_key, look_for_keys=False)
-        #client.connect(hostname=hostname, port=port, username=username, password=password, banner_timeout=10, auth_timeout=10, timeout=10, allow_agent=False, look_for_keys=False)
-
-        # Se a conexão foi bem sucedida, imprimir mensagem de sucesso
-        #print("Conexão SSH bem-sucedida!")
-
         return client
-
     except Exception as e:
-        # Se ocorrer algum erro, imprimir mensagem de falha
-        print("Falha ao conectar via SSH:", e)
+        log_error("Falha ao conectar via SSH: " + str(e))
         return None
 
 
 try:
+    log_message("Iniciando o serviço...")
 
     retConfig = fDados.fConfig()
-    #ambiente = "HOMOLOGACAO"
-    ambiente = "PRODUCAO"
+    ambiente = "PRODUCAO"  # Definido manualmente para teste, pode ser modificado conforme necessário
+    log_message("Ambiente definido: " + ambiente)
 
-    totDias         = retConfig["alianca"]["qtosDias"]
-    removerArq      = retConfig["alianca"]["removerArqOrigem"]
-    sDestino        = retConfig["alianca"]["SOCEN"]["destino"]
-    sPrefixo        = retConfig["alianca"]["SOCEN"]["prefixoArq"]
-    sDestinoNOTFIS  = retConfig["alianca"]["NOTFIS"]["destino"]
-    sPrefixoNOTFIS  = retConfig["alianca"]["NOTFIS"]["prefixoArq"]
-    sOutBound       = retConfig["alianca"]["outbound_chave"]
-    sHost           = retConfig["alianca"][ambiente]["host_chave"]
-    sPort           = retConfig["alianca"][ambiente]["port_chave"]
-    sUser           = retConfig["alianca"][ambiente]["user_chave"]
-    sFile           = retConfig["alianca"][ambiente]["file_chave"]
-    sPwd            = retConfig["alianca"][ambiente]["pwd_chave"]
+    # Recuperando configurações do arquivo de configuração
+    totDias = retConfig["alianca"]["qtosDias"]
+    removerArq = retConfig["alianca"]["removerArqOrigem"]
+    sDestino = retConfig["alianca"]["SOCEN"]["destino"]
+    sPrefixo = retConfig["alianca"]["SOCEN"]["prefixoArq"]
+    sDestinoNOTFIS = retConfig["alianca"]["NOTFIS"]["destino"]
+    sPrefixoNOTFIS = retConfig["alianca"]["NOTFIS"]["prefixoArq"]
+    sOutBound = retConfig["alianca"]["outbound_chave"]
+    sHost = retConfig["alianca"][ambiente]["host_chave"]
+    sPort = retConfig["alianca"][ambiente]["port_chave"]
+    sUser = retConfig["alianca"][ambiente]["user_chave"]
+    sFile = retConfig["alianca"][ambiente]["file_chave"]
+    sPwd = retConfig["alianca"][ambiente]["pwd_chave"]
+    log_message("Variáveis de configuração definidas.")
 
     # Testar a conexão SSH
     client = test_ssh_connection(sHost, sPort, sUser, sFile, sPwd, sPwd)
+    if client:
+        log_message("Conexão SSH bem-sucedida.")
+    else:
+        log_message("Não foi possível estabelecer uma conexão SSH.")
 
     if client:
-
-        print("Conectado...")
+        log_message("Conectado...")
 
         sftp = client.open_sftp()
         arqs = sftp.listdir(sOutBound)
 
         # Verificar se a pasta de destino não está vazia
         if arqs:
-            print("Foram encontrados arquivos na pasta de destino.")
-            for arquivo in arqs:
-                print(arquivo)
+            log_message("Foram encontrados arquivos na pasta de destino.")
             # Adicione aqui qualquer ação que você queira realizar se houver arquivos na pasta de destino
         else:
-            print("Nenhum arquivo encontrado na pasta de destino.")
+            log_message("Nenhum arquivo encontrado na pasta de destino.")
 
         hoje = dt.datetime.today()
         datahora = hoje.strftime('%d/%m/%y %H:%M')
@@ -84,9 +94,6 @@ try:
             contDia += 1
             diames = data.strftime('%d%m')
             diamesano = data.strftime('%d%m%y')
-            diabarrames = data.strftime('%d/%m')
-
-            print(diamesano)
 
             nomeInicialArq = sPrefixo + str(diamesano)
             nomeInicialArqNOTFIS = sPrefixoNOTFIS + str(diamesano)
@@ -107,12 +114,12 @@ try:
                             copiado = 1
 
                     if copiado == 0:
-                        print("Arquivo " + image + " sendo copiado...")
+                        log_message("Arquivo " + image + " sendo copiado...")
                         sftp.get(sOutBound + image, sDestino + image)
                         copiados = copiados + 1
                         totarqs = totarqs + 1
                     else:
-                        print("Arquivo " + image + " já copiado.")
+                        log_message("Arquivo " + image + " já copiado.")
                         totarqs = totarqs + 1
 
                 if nomeInicialArqNOTFIS in image:
@@ -122,23 +129,22 @@ try:
                             copiadoNOTFIS = 1
 
                     if copiadoNOTFIS == 0:
-                        print("Arquivo " + image + " sendo copiado...")
+                        log_message("Arquivo " + image + " sendo copiado...")
                         sftp.get(sOutBound + image, sDestinoNOTFIS + image)
                         copiadosNOTFIS = copiadosNOTFIS + 1
                         totarqsNOTFIS = totarqsNOTFIS + 1
                     else:
-                        print("Arquivo " + image + " já copiado.")
+                        log_message("Arquivo " + image + " já copiado.")
                         totarqsNOTFIS = totarqsNOTFIS + 1
 
             with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-                arqlog.write('[ (' + diabarrames + ') SOCEN ' + str(copiados) + '/' + str(totarqs))
-                arqlog.write(' NOTFIS ' + str(copiadosNOTFIS) + '/' + str(totarqsNOTFIS) + ' ]')
+                arqlog.write('[SOCEN ' + str(copiados) + '/' + str(totarqs))
+                arqlog.write(' NOTFIS ' + str(copiadosNOTFIS) + '/' + str(totarqsNOTFIS) + '] ')
 
         dd = dt.timedelta(totDias)
         data = dt.datetime.now() - dd
 
         diamesano = data.strftime('%d%m%y')
-        dataremove = data.strftime('%d/%m/%y')
 
         nomeInicialArq = sPrefixo + str(diamesano)
         nomeInicialArqNOTFIS = sPrefixoNOTFIS + str(diamesano)
@@ -158,26 +164,21 @@ try:
                     sftp.remove(sOutBound + image)
 
         with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-            arqlog.write('- Foram removidos em ' + str(dataremove) + ' ')
-            arqlog.write('- SOCEN ' + str(removidos) + " arqs ")
-            arqlog.write('- NOTFIS ' + str(removidosNOTFIS) + ' arqs.' + chr(13))
+            arqlog.write('- Removidos SOCEN: ' + str(removidos))
+            arqlog.write(' NOTFIS: ' + str(removidosNOTFIS) + '\n')
 
         sftp.close()
         client.close()
 
+    log_message("Serviço finalizado com sucesso.")
+
 except FileNotFoundError:
-    erro = 'Arquivo dados.json não encontrado na pasta do programa Executável.'
-    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-        arqlog.write(str(dt.date.today()) + ' Erro: ' + erro + chr(13))
+    log_error('Arquivo dados.json não encontrado na pasta do programa Executável.')
 except PermissionError:
-    erro = 'Usuário sem permissão para abrir arquivo dados.json.'
-    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-        arqlog.write(str(dt.date.today()) + ' Erro: ' + erro + chr(13))
+    log_error('Usuário sem permissão para abrir arquivo dados.json.')
 except IOError:
-    erro = 'Erro de E/S.'
-    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-        arqlog.write(str(dt.date.today()) + ' Erro: ' + erro + chr(13))
+    log_error('Erro de E/S.')
 except json.JSONDecodeError:
-    erro = 'Formato do arquivo dados.json inválido. Erro de decoder.'
-    with open(arqLog, 'a', encoding='utf-8-sig') as arqlog:
-        arqlog.write(str(dt.date.today()) + ' Erro: ' + erro + chr(13))
+    log_error('Formato do arquivo dados.json inválido. Erro de decoder.')
+except Exception as e:
+    log_error(str(e))
